47a77449d990efc3714c5872e88e38373ea06b0b
coder-lloyd <coder.lloyd01@gmail.com>
2025-07-27 15:24:24 +0900

    [Unity - Match3] ## Addon.Tilemap * Tilemap 컨트롤 유틸 추가

---
+++ Unity/Match3/Assets/Scripts/addon/Tilemap.EX.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace LLOYD.Match3.Addon
+{
+    namespace Tilemap
+    {
+        static class Extensions
+        {
+            public static void Loop_Tiles(this UnityEngine.Tilemaps.Tilemap __timlemap)
+            {
+                // 타일맵 전체 크기 가져오기
+                BoundsInt bounds = __timlemap.cellBounds;
+                TileBase[] allTiles = __timlemap.GetTilesBlock(bounds);
+
+                // 모든 타일 순회
+                int count = 0;
+                for (int x = 0; x < bounds.size.x; x++)
+                {
+                    for (int y = 0; y < bounds.size.y; y++)
+                    {
+                        TileBase tile = allTiles[x + y * bounds.size.x];
+                        if (tile != null)
+                        {
+                            // 타일 처리 로직 (예: 타일 정보 출력)
+                            Debug.Log($"타일 위치: ({x}, {y}), 타일 이름: {tile.name}");
+
+                            count += 1;
+                        }
+                    }
+                }
+                Debug.Log($"Stage.Loop_Tiles(): gem 개수= {count}");
+            }
+            public static void Loop_Tiles_byGetTile(this UnityEngine.Tilemaps.Tilemap __timlemap)
+            {
+                // 타일맵 전체 크기 가져오기
+                BoundsInt bounds = __timlemap.cellBounds;
+
+                // 모든 타일 순회
+                int count = 0;
+                for (int x = bounds.x; x < bounds.xMax; x++)
+                {
+                    for (int y = bounds.y; y < bounds.yMax; y++)
+                    {
+                        Vector3Int cellPosition = new Vector3Int(x, y, 0);
+                        TileBase tile = __timlemap.GetTile(cellPosition);
+
+                        if (tile != null)
+                        {
+                            // 타일 처리 로직 (예: 타일 정보 출력)
+                            Debug.Log($"타일 위치: ({x}, {y}), 타일 이름: {tile.name}");
+                            count += 1;
+                        }
+                    }
+                }
+                Debug.Log($"Stage.Loop_Tiles_byGetTile(): gem 개수= {count}");
+            }
+            public static void Loop_Tiles_byGrid(this UnityEngine.Tilemaps.Tilemap __timlemap, Grid __grid)
+            {
+                // 타일맵 전체 크기 가져오기
+                BoundsInt bounds = __timlemap.cellBounds;
+                //var grid = this.GetComponent<Grid>();
+
+                // 월드 좌표 순회 예시
+                int count = 0;
+                for (int x = bounds.xMin; x < bounds.xMax; x++)
+                {
+                    for (int y = bounds.yMin; y < bounds.yMax; y++)
+                    {
+                        Vector3Int cellPosition = new Vector3Int(x, y, 0);
+                        Vector3 worldPosition = __grid.GetCellCenterWorld(cellPosition);
+
+
+                        // 월드 좌표에서 타일 처리 로직
+                        //string strlog = $"셀 위치: ({x}, {y}), 월드 위치: {worldPosition}";
+
+                        TileBase tile = __timlemap.GetTile(cellPosition);
+                        if (tile != null)
+                        {
+                            //strlog += $", 타일 이름: {tile.name}";
+                            Debug.Log($"셀 위치: ({x}, {y}), 월드 위치: {worldPosition}, 타일 이름: {tile.name}");
+                            count += 1;
+                        }
+
+                        //Debug.Log(strlog);
+                    }
+                }
+                Debug.Log($"Stage.Loop_Tiles_byGrid(): gem 개수= {count}");
+            }
+
+            public static void Erasse_Gem(this UnityEngine.Tilemaps.Tilemap __timlemap, Vector3Int __pos)
+            {
+                //TMAP_Gems.GetTile(new Vector3Int(0, 0, 0));
+                __timlemap.SetTile(__pos, null);
+            }
+        }
+    }
+}
--- Unity/Match3/Assets/Tiles/Stage.cs
+++ Unity/Match3/Assets/Tiles/Stage.cs
@@ -3,6 +3,8 @@ using UnityEngine.Tilemaps;
 
 namespace LLOYD.Match3
 {
+    using Addon.Tilemap;
+
     public class Stage : MonoBehaviour
     {
         [SerializeField] Tilemap TMAP_Gems = null;
@@ -13,98 +15,12 @@ namespace LLOYD.Match3
             //TMAP_Gems.ClearAllTiles();
             Debug.Log($"Board.Start(): TMAP_Gems= {TMAP_Gems.GetUsedTilesCount()}");
 
-            Loop_Tiles();//[Sprite_GreenItem] (3, 5)
-            Loop_Tiles_byGetTile();//[Sprite_GreenItem] (-1, 1)
-            Loop_Tiles_byGrid();//[Sprite_GreenItem] 셀 위치: (-1, 1), 월드 위치: (-0.50, 1.50, 0.00)
+            TMAP_Gems.Loop_Tiles();//[Sprite_GreenItem] (3, 5)
+            TMAP_Gems.Loop_Tiles_byGetTile();//[Sprite_GreenItem] (-1, 1)
+            TMAP_Gems.Loop_Tiles_byGrid(this.GetComponent<Grid>());//[Sprite_GreenItem] 셀 위치: (-1, 1), 월드 위치: (-0.50, 1.50, 0.00)
 
-            //Erasse_Gem(new Vector3Int(0, 0, 0));
-        }
-
-        void Loop_Tiles()
-        {
-            // 타일맵 전체 크기 가져오기
-            BoundsInt bounds = TMAP_Gems.cellBounds;
-            TileBase[] allTiles = TMAP_Gems.GetTilesBlock(bounds);
-
-            // 모든 타일 순회
-            int count = 0;
-            for (int x = 0; x < bounds.size.x; x++)
-            {
-                for (int y = 0; y < bounds.size.y; y++)
-                {
-                    TileBase tile = allTiles[x + y * bounds.size.x];
-                    if (tile != null)
-                    {
-                        // 타일 처리 로직 (예: 타일 정보 출력)
-                        Debug.Log($"타일 위치: ({x}, {y}), 타일 이름: {tile.name}");
-
-                        count += 1;
-                    }
-                }
-            }
-            Debug.Log($"Stage.Loop_Tiles(): gem 개수= {count}");
-        }
-        void Loop_Tiles_byGetTile()
-        {
-            // 타일맵 전체 크기 가져오기
-            BoundsInt bounds = TMAP_Gems.cellBounds;
-
-            // 모든 타일 순회
-            int count = 0;
-            for (int x = bounds.x; x < bounds.xMax; x++)
-            {
-                for (int y = bounds.y; y < bounds.yMax; y++)
-                {
-                    Vector3Int cellPosition = new Vector3Int(x, y, 0);
-                    TileBase tile = TMAP_Gems.GetTile(cellPosition);
-
-                    if (tile != null)
-                    {
-                        // 타일 처리 로직 (예: 타일 정보 출력)
-                        Debug.Log($"타일 위치: ({x}, {y}), 타일 이름: {tile.name}");
-                        count += 1;
-                    }
-                }
-            }
-            Debug.Log($"Stage.Loop_Tiles_byGetTile(): gem 개수= {count}");
-        }
-        void Loop_Tiles_byGrid()
-        {
-            // 타일맵 전체 크기 가져오기
-            BoundsInt bounds = TMAP_Gems.cellBounds;
-            var grid = this.GetComponent<Grid>();
-
-            // 월드 좌표 순회 예시
-            int count = 0;
-            for (int x = bounds.xMin; x < bounds.xMax; x++)
-            {
-                for (int y = bounds.yMin; y < bounds.yMax; y++)
-                {
-                    Vector3Int cellPosition = new Vector3Int(x, y, 0);
-                    Vector3 worldPosition = grid.GetCellCenterWorld(cellPosition);
-                    
-
-                    // 월드 좌표에서 타일 처리 로직
-                    //string strlog = $"셀 위치: ({x}, {y}), 월드 위치: {worldPosition}";
-
-                    TileBase tile = TMAP_Gems.GetTile(cellPosition);
-                    if (tile != null)
-                    {
-                        //strlog += $", 타일 이름: {tile.name}";
-                        Debug.Log($"셀 위치: ({x}, {y}), 월드 위치: {worldPosition}, 타일 이름: {tile.name}");
-                        count += 1;
-                    }
-
-                    //Debug.Log(strlog);
-                }
-            }
-            Debug.Log($"Stage.Loop_Tiles_byGrid(): gem 개수= {count}");
-        }
-
-        void Erasse_Gem(Vector3Int __pos)
-        {
-            //TMAP_Gems.GetTile(new Vector3Int(0, 0, 0));
-            TMAP_Gems.SetTile(__pos, null);
+            //TMAP_Gems.Erasse_Gem(new Vector3Int(0, 1, 0));
+            //TMAP_Gems.Erasse_Gem(new Vector3Int(1, 2, 0));
         }
 
         // Update is called once per frame
