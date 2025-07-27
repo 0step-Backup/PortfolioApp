ea6f612ad989e8eb45956b6185644324723df918
coder-lloyd <coder.lloyd01@gmail.com>
2025-07-27 15:31:42 +0900

    [Unity - Match3] ## Common.Types * Gem 타입 추가

---
+++ Unity/Match3/Assets/Scripts/common/Types.cs
@@ -0,0 +1,10 @@
+namespace LLOYD.Match3.Common
+{
+    public class Types
+    {
+        public enum Gem { NONE = -1,
+            random = 0,
+            blue = 1, green, purple, red, white, yellow,
+        }
+    }
+}
