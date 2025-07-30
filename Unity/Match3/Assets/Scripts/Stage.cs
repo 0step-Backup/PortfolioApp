008bc5acef947e6bfd6f61ff0ce708c6e6cc5b0d
coder-lloyd <coder.lloyd01@gmail.com>
2025-07-30 18:47:17 +0900

    [Unity - Match3] ## Stage * Gem 스왑 Input 체크  *

--- Unity/Match3/Assets/Scripts/Stage.cs
+++ Unity/Match3/Assets/Scripts/Stage.cs
@@ -16,7 +16,6 @@ namespace LLOYD.Match3
 
         bool _is_picked = false;
         Vector3 _cellv3_pick = Vector3.zero;
-        Vector3 _cellv3_target = Vector3.zero;
 
         // Start is called before the first frame update
         void Start()
@@ -49,32 +48,47 @@ namespace LLOYD.Match3
 
         public void Enter_Gem(Node.Gem __gem)
         {
-            Debug.Log($"<color=red>Stage.Enter_Gem</color>({__gem.transform.position})");
+            //Debug.Log($"<color=red>Stage.Enter_Gem</color>({__gem.transform.position})");
 
-            if(_is_picked)
+            if(_is_picked
+                && _cellv3_pick != __gem.transform.position
+                )
             {
+                //if(인접한 블럭이면)
+                //matching
+
+                _is_picked = false;
+                Debug.Log($"<color=cyan>target gem: {__gem.TYPE}</color>, ({_cellv3_pick.x}, {_cellv3_pick.y})");
             }
         }
         public void Out_Gem(Node.Gem __gem)
         {
-            Debug.Log($"<color=grey>Stage.Out_Gem</color>({__gem.transform.position})");
+            //Debug.Log($"<color=grey>Stage.Out_Gem</color>({__gem.transform.position})");
         }
 
         public void Push_Gem(Node.Gem __gem)
         {
-            Debug.Log($"<color=green>Stage.Push_Gem</color>({__gem.transform.position})");
+            //Debug.Log($"<color=green>Stage.Push_Gem</color>({__gem.transform.position})");
 
             if (!_is_picked)
             {
                 _is_picked = true;
 
-                _cellv3_pick = __gem.transform.position;                
+                _cellv3_pick = __gem.transform.position;
+                Debug.Log($"<color=green>pick gem: {__gem.TYPE}</color>, ({_cellv3_pick.x}, {_cellv3_pick.y})");
             }
         }
 
         public void Release_Gem(Node.Gem __gem)
         {
-            Debug.Log($"<color=black>Stage.Release_Gem</color>({__gem.transform.position})");
+            //Debug.Log($"<color=black>Stage.Release_Gem</color>({__gem.transform.position})");
+
+            if(_is_picked &&
+                _cellv3_pick == __gem.transform.position)
+            {
+                _is_picked = false;
+                Debug.Log($"<color=grey>pick release gem: {__gem.TYPE}</color>, ({_cellv3_pick.x}, {_cellv3_pick.y})");
+            }
         }
 
         // Update is called once per frame
