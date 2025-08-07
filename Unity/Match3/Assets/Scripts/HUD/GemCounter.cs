19540fbc049a7893c033f9ac73a29959605d5a7c
coder-lloyd <coder.lloyd01@gmail.com>
2025-08-07 19:18:33 +0900

    [Unity - Match3] ## HUD * GemCounter  * 잼 카운터

---
+++ Unity/Match3/Assets/Scripts/HUD/GemCounter.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LLOYD.Match3.HUD.NODE
+{
+    public class GemCounter : MonoBehaviour
+    {
+        [SerializeField] Text TXT = null;
+
+        int _count = 0;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Set_Count(0);
+        }
+
+        public void Reset_Counting() => Set_Count(0);
+
+        public void Counting() => Set_Count(_count + 1);
+
+        void Set_Count(int __count)
+        {
+            _count = __count;
+            TXT.text = __count.ToString();
+        }
+    }
+}
